43f3a5d415c07a57de9137e6aaece031855f53a1
jeanmarcosilva-hash <jean.marco.silva@hotmail.com>
2026-04-02 19:30:27 -0300

    projeto finalizado

--- main.js
+++ main.js
@@ -2,22 +2,30 @@ const form = document.getElementById('contact-form');
 const nameInput = document.getElementById('name');
 const phoneInput = document.getElementById('phone');
 const categoryInput = document.getElementById('class-id');
-const table = document.querySelector('.contacts-table tbody');
+const deleteAllButton = document.getElementById('clear-btn');
+const table = document.querySelector('.contacts-table');
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  const name = nameInput.value;
-  const phone = phoneInput.value;
-  const category = categoryInput.value;
-
-  const newRow = document.createElement('tr');
-  newRow.innerHTML = `
-    <td>${name}</td>
-    <td>${phone}</td>
-    <td>${category}</td>
+deleteAllButton.addEventListener('click', function() {
+  
+  table.innerHTML = `
+    <tr>
+      <th></th>
+      <th></th>
+      <th></th>
+    </tr>
   `;
-  table.appendChild(newRow);
-
-  form.reset();
+});
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const name = nameInput.value;
+    const phone = phoneInput.value;
+    const category = categoryInput.value;
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+        <td>${name}</td>
+        <td>${phone}</td>
+        <td>${category}</td>
+    `;
+    table.appendChild(newRow); 
+    form.reset();
 });
